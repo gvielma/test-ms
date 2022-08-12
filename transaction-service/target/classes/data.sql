caf5764f67a9263350baf820e3ea43c46755c009
gvielma <gvielma87@gmail.com>
2022-08-12 01:34:10 -0500

    config git ignore in transaction-service project

--- transaction-service/target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-create table movimientos(
-     
-id INTEGER auto_increment NOT NULL PRIMARY KEY, 
-numero_cuenta VARCHAR (255) NOT NULL,
-cliente_id INTEGER NOT NULL,
-fecha TIMESTAMP NOT NULL,
-saldo_inicial double NOT NULL,
-saldo_disponible double NOT NULL,
-movimiento double NOT NULL);
